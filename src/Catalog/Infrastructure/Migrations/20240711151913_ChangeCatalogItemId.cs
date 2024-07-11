4d14dc94b0441c8806ec3d4c84a80199a25f7376
Nabi Karampoor <thisisnabi@outlook.com>
2024-07-11 18:52:27 +0330

    Add Change item in catalog schema

---
+++ src/Catalog/Infrastructure/Migrations/20240711151913_ChangeCatalogItemId.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCatalogItemId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CatalogItems",
+                schema: "catalog",
+                table: "CatalogItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CatalogItems_Slug",
+                schema: "catalog",
+                table: "CatalogItems");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "catalog",
+                table: "CatalogItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CatalogItems",
+                schema: "catalog",
+                table: "CatalogItems",
+                column: "Slug");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CatalogItems",
+                schema: "catalog",
+                table: "CatalogItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "catalog",
+                table: "CatalogItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CatalogItems",
+                schema: "catalog",
+                table: "CatalogItems",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_Slug",
+                schema: "catalog",
+                table: "CatalogItems",
+                column: "Slug");
+        }
+    }
+}
